00646f70aefee4486e47779e03858524bee2815f
styts <kirstyts@gmail.com>
2013-07-26 16:06:42 +0200

    phone hole

--- phone_holder.py
+++ phone_holder.py
@@ -1,22 +1,43 @@
 import os
 
-from solid import circle, hull
-from solid.utils import scad_render_to_file, right
+from solid import circle, square, hull, linear_extrude
+from solid.utils import *
 
 SEGMENTS = 20
 
-radius = 20
-length = 3 * radius
+phone_thickness = 13.2
+phone_width = 60.4
+
+radius = phone_width / 4
+screw_hole_radius = 3
+screw_hole_shift = radius / 1.5
+layer_length = phone_width - radius / 2
+material_height = 6
+
+
+def phone_hole():
+    hole = square([phone_width, phone_thickness])
+    x_offset = (layer_length - phone_width) / 2
+    return right(x_offset)(back(phone_thickness / 2)(hole))
 
 
 def layer():
-    body = hull()(circle(r=radius) + right(length)(circle(r=radius)))
-    hole_radius = radius / 4
-    holes = [
-        circle(hole_radius),
-        right(length)(circle(hole_radius))
+    # streched oval
+    body = hull()(circle(radius) + right(layer_length)(circle(radius)))
+
+    # used to hold the layers together
+    screw_holes = [
+        left(screw_hole_shift)(circle(screw_hole_radius)),
+        right(screw_hole_shift + layer_length)(circle(screw_hole_radius))
     ]
-    return body - holes
+    body = body - screw_holes
+
+    # phone slides into this hole
+    body = body - phone_hole()
+
+    # 2D -> 3D
+    body = linear_extrude(height=material_height)(body)
+    return body
 
 
 def assembly():
