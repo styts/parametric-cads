500ac70b7b969b70f0879177fcc2b90b4b5e2eab
styts <kirstyts@gmail.com>
2013-07-26 19:45:23 +0200

    an OOP-like abstraction

--- phone_holder.py
+++ phone_holder.py
@@ -1,33 +1,42 @@
 import os
 
 from solid import circle, square, hull, linear_extrude
-from solid.utils import *
+from solid.utils import left, right, back, up, union, scad_render_to_file
 
-SEGMENTS = 20
+# rendering parameters
+SEGMENTS = 50  # increase for more smoothness
+layer_gap = 0.0  # > 0 to visualize layer breaks
 
 phone_thickness = 13.2
 phone_width = 60.4
 
-radius = phone_width / 4
+radius = phone_width / 4  # curvature left/right
 screw_hole_radius = 3  # 3mm screws hold the layers together
 screw_hole_shift = radius / 1.5
-layer_length = phone_width
 
 material_height = 6
-layer_gap = 0.0
-
-top_layers = 2
 
 
 class Layer(object):
-    def render(self):
-        # streched oval
-        body = hull()(circle(radius) + right(layer_length)(circle(radius)))
+    def make_hole(self):
+        try:
+            getattr(self, 'hole')
+        except:
+            return
+
+        ho = square([self.hole.x, self.hole.y])
+        body = right(self.hole.x_offset)(back(self.hole.y_offset())(ho))
+        body = linear_extrude(height=material_height)(body)
+        return body
+
+    def __init__(self):
+        # stretched oval
+        body = hull()(circle(radius) + right(phone_width)(circle(radius)))
 
         # used to hold the layers together
         screw_holes = [
             left(screw_hole_shift)(circle(screw_hole_radius)),
-            right(screw_hole_shift + layer_length)(circle(screw_hole_radius))
+            right(screw_hole_shift + phone_width)(circle(screw_hole_radius))
         ]
         body = body - screw_holes
 
@@ -37,48 +46,38 @@ class Layer(object):
         # 2D -> 3D
         self.body = linear_extrude(height=material_height)(body)
 
+        # possible hole
+        self.body -= self.make_hole()
+
+    def get_body(self):
+        return self.body
+
+
+class Hole(object):
+    def __init__(self, x_offset, x, y):
+        self.x = x
+        self.y = y
+        self.x_offset = x_offset
+
+    def y_offset(self):
+        return 0
+
+
+class CenteredHole(Hole):
+    def y_offset(self):
+        return self.y / 2
+
 
 class TopLayer(Layer):
-    def phone_hole(self):
-        hole = square([phone_width, phone_thickness])
-        x_offset = (phone_width / 4)
-        body = right(x_offset)(back(phone_thickness / 2)(hole))
-        body = linear_extrude(height=material_height)(body)
-        return body
-
-    def render(self):
-        super(TopLayer, self).render()
-        self.body -= self.phone_hole()
+    hole = CenteredHole(phone_width / 4, phone_width, phone_thickness)
 
 
 class MidLayer(Layer):
-    hole_w = 10.8
-    hole_h = 7.2
-
-    def adapter_hole(self):
-        hole = square([self.hole_w, self.hole_h])
-        x_offset = layer_length / 2 + self.hole_w
-        body = right(x_offset)(
-            back(self.hole_h / 2)(hole))
-        return linear_extrude(height=material_height)(body)
-
-    def render(self):
-        super(MidLayer, self).render()
-        self.body -= self.adapter_hole()
+    hole = CenteredHole(phone_width / 2 + 10.8, 10.8, 7.2)
 
 
 class BottomLayer(Layer):
-    def cable_hole(self):
-        w = 3
-        x_offset = layer_length / 2 + radius
-        sq = square([w, radius])
-        body = right(x_offset)(sq)
-        body = linear_extrude(height=material_height)(body)
-        return body
-
-    def render(self):
-        super(BottomLayer, self).render()
-        self.body -= self.cable_hole()
+    hole = Hole(phone_width / 2 + radius, 3, radius)
 
 
 def assembly():
@@ -100,9 +99,8 @@ def assembly():
     layers = []
     for i, l in enumerate(ls):
         l_inst = l()
-        l_inst.render()
         layers.append(
-            up(i * (material_height + layer_gap))(l_inst.body)
+            up(i * (material_height + layer_gap))(l_inst.get_body())
         )
 
     return union()(layers)
