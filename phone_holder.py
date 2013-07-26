b47b148d72a38790cea40a30e4d27bfda49236cd
styts <kirstyts@gmail.com>
2013-07-26 12:50:46 +0200

    layer with 2 screwholes

--- phone_holder.py
+++ phone_holder.py
@@ -1,19 +1,31 @@
 import os
 
-from solid import cylinder
-from solid.utils import scad_render_to_file
+from solid import circle, hull
+from solid.utils import scad_render_to_file, right
 
-SEGMENTS = 43
+SEGMENTS = 20
+
+radius = 20
+length = 3 * radius
 
 
-def piece():
-    cyl = cylinder(r=5, h=15, center=True) - cylinder(r=4, h=16, center=True)
-    return cyl
+def layer():
+    body = hull()(circle(r=radius) + right(length)(circle(r=radius)))
+    hole_radius = radius / 4
+    holes = [
+        circle(hole_radius),
+        right(length)(circle(hole_radius))
+    ]
+    return body - holes
+
+
+def assembly():
+    return layer()
 
 if __name__ == '__main__':
     out_dir = os.path.join(os.curdir, 'output')
     file_out = os.path.join(out_dir, '%s.scad' % __file__[:-3])
-    a = piece()
+    a = assembly()
     print "SCAD file written to: %s" % file_out
     scad_render_to_file(a, file_out, file_header='$fn = %s;' % SEGMENTS)
 
